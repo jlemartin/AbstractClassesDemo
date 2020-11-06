821ef8fc3d6250ce1605d1373d0526bae69fa933
John Martin <jlemartin7@gmail.com>
2020-11-06 13:17:09 -0600

    updating with GetType() calls

--- ConsoleUI/Car.cs
+++ ConsoleUI/Car.cs
@@ -18,7 +18,7 @@ namespace ConsoleUI
 
         public override void DriveAbstract()
         {
-            Console.WriteLine("In Abstract Drive!");
+            Console.WriteLine($"In Abstract Drive in a {GetType().Name} that is a {GetType().BaseType.Name}");
         }
     }
 }
